d9be5db4425c1d5bb8d01bedccf96cd5e07c8b6d
David Kalatzis <davidkalatzis1999@hotmail.com>
2021-09-13 10:36:50 +0200

    geg is cool.

--- Week 37/mqtt-assignment/src/app.module.ts
+++ Week 37/mqtt-assignment/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MqttController } from './mqtt.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MQTT_CLIENT',
+        transport: Transport.MQTT,
+        options: {
+          host: 'mqtts://a8abef4e9bd641b69b6ea2c4de9f512f.s1.eu.hivemq.cloud:8883',
+          username: 'skelly',
+          password: 'SKELLYskelly11!'
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController, MqttController],
   providers: [AppService],
 })
 export class AppModule {}
--- Week 37/mqtt-assignment/src/main.ts
+++ Week 37/mqtt-assignment/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
+  app.connectMicroservice({
+    transport: Transport.MQTT,
+    options: {
+      host: 'mqtts://a8abef4e9bd641b69b6ea2c4de9f512f.s1.eu.hivemq.cloud:8883',
+      username: 'skelly',
+      password: 'SKELLYskelly11!'
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
